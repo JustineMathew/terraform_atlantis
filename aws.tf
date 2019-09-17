c67cfbdb186e691874bc791e2478609f081ee330
justinemathew <justine.mathew@coviam.com>
2019-09-17 14:18:11 +0530

    first commit

--- aws.tf
+++ aws.tf
@@ -18,7 +18,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "web1" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 
